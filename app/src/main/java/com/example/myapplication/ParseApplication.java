b6a7543bfb2c04a3fe2854bbb7aec18b4bc59aab
Anika Patel <anikapatel@fb.com>
2022-06-13 16:16:07 -0700

    Step 4 Push

--- app/src/main/java/com/example/myapplication/ParseApplication.java
+++ app/src/main/java/com/example/myapplication/ParseApplication.java
@@ -3,11 +3,16 @@ package com.example.myapplication;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     @Override
     public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("q2UI2EGDv8gg7Exrx5VLmdpahhInj7F5aPzjlB12")
                 .clientKey("hGwd88UelQzHPBPB5TJkfKxnOa04ZiJnrOlHX9Xy")
